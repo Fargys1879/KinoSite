ff88b67c7d1aa607a86cd0f4b48f7b207b53ebe4
Fargys1879 <63678494+Fargys1879@users.noreply.github.com>
2020-05-18 07:16:57 +0300

    Терминатор

--- django_movie/movies/views.py
+++ django_movie/movies/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render
 from django.views.generic.base import View
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Movie
 
 # Create your views here.
-class MovieView(View):
-    """Список Фильмов"""
+"""class MovieView(View):
     def get(self, request):
         movies = Movie.objects.all()
-        return render(request,"movies/movies.html", {'movie_list' : movies})
+        return render(request,"movies/movies.html", {'movie_list' : movies})"""
 
-class MovieDetailView(View):
+class MovieView(ListView):
+    """Список Фильмов"""
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)  
+
+class MovieDetailView(DetailView):
     """Полное описание Фильма"""
-    def get(self, request, slug):
-        movie = Movie.objects.get(url = slug)
-        return render(request,"movies/movie_detail.html", {'movie' : movie})
+    model = Movie
+    slug_field = "url"
     
