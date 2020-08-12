ea769cefbe0e913a1c8e6d146b990d385226de52
Fargys1879 <63678494+Fargys1879@users.noreply.github.com>
2020-08-12 17:05:29 +0300

    heroku
    heroku

--- django_movie/django_movie/settings.py
+++ django_movie/django_movie/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!h3sw$d8*-f@^$$7$t&wuw_&&r*+vvk4l%-@jbn!bqr1@&c%ef'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [heroku.com]
 
 
 # Application definition
